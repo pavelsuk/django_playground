d3a79a8a357b20c86cb6740922db94f9fd1c1b99
Pavel Suk <suk.pavel@gmail.com>
2021-11-05 17:16:00 +0100

    Adding one more path

--- 01_official_tutorial/pasusite/polls2/urls.py
+++ 01_official_tutorial/pasusite/polls2/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('hopla', views.hopla, name='hopla'),
+    path('lopla', views.hopla, name='lopla'),
 ]
--- 01_official_tutorial/pasusite/polls2/views.py
+++ 01_official_tutorial/pasusite/polls2/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index. And it's located in pools2 directory!")
+
+def hopla(request):
+    return HttpResponse("Hopla lopla")
